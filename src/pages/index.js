9c78a3b8b127971de11f62c8fadb26330a10d7d8
Isaac Martin <hey@isaacmartin.co>
2022-04-23 15:09:15 -0400

    add external

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,11 +5,15 @@ import { GatsbyImage } from "gatsby-plugin-image"
 import { useThemeUI, Grid, Heading } from "theme-ui"
 import { Link } from "gatsby"
 
+const getUrl = project => {
+  if (project.url) return { url: project.url, isExternal: true }
+  return { url: project.slug.current, isExternal: false }
+}
+
 const IndexPage = props => {
   const {
     theme: { space },
   } = useThemeUI()
-  console.log(props.data)
   const menuItems = props.data.sanityMenu.homeFeaturedItems
 
   return (
@@ -25,17 +29,15 @@ const IndexPage = props => {
         pb={4}
       >
         {menuItems.map(({ image, project }) => {
-          // console.log(item)
-          // if (!item.id) return null
-
           return (
             <Link
+              target={getUrl(project).isExternal ? "_blank" : ""}
               style={{
                 color: `black`,
                 textDecoration: `none`,
                 fontWeight: `bold`,
               }}
-              to={project.slug.current}
+              to={getUrl(project).url}
             >
               <GatsbyImage
                 image={image.asset.gatsbyImageData}
@@ -74,6 +76,11 @@ export const homeQuery = graphql`
             slug {
               current
             }
+          }
+          ... on SanityCustomLink {
+            id
+            title
+            url
           }
         }
       }
